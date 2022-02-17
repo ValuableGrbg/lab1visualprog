6dd25ad22a9c5c0b6b5cb174f6f2015881d2fa91
Vadim <Vadim@DESKTOP-VP29V00>
2022-02-17 15:05:41 +0700

    Добавьте файлы проекта.

---
+++ lab1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace lab1
+{
+    public class HW1 
+    {
+        public static long QueueTime(int[] customers, int n)
+        {
+            int[] a = new int[n];
+            int m;
+            int minIndex;
+
+            for (int i = 0; i < n; i++) {
+                a[i] = 0; 
+                a[i] += customers[i];
+            }
+            for (int i = n; i < customers.Length; i++) {
+                m = a.Min();
+                minIndex = Array.IndexOf(a, m);
+                a[minIndex] += customers[i];
+            }
+            m = a.Max();
+            Console.WriteLine(m);
+            return 0;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HW1.QueueTime(new int[] { 5, 3, 4 }, 1);
+            HW1.QueueTime(new int[] { 10, 2, 3, 3 }, 2);
+            HW1.QueueTime(new int[] { 2, 3, 10 }, 2);
+
+        }
+    }
+}
